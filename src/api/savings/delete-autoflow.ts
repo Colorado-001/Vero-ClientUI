54e6140268735b9629c02e8250ee9cd49226a0aa
crazy_dev <pelumidevelops18@gmail.com>
2025-10-21 23:43:48 +0100

    hotfix

--- src/api/savings/delete-autoflow.ts
+++ src/api/savings/delete-autoflow.ts
@@ -1,7 +1,7 @@
 import { axiosInstance } from "../client";
 import endpoints from "../endpoints";
 
-export const deleteAutoFlow = async (id: string) => {
+export const deleteAutoFlow = async (id: number) => {
   await axiosInstance.delete(endpoints.autoflow(), {
     params: {
       id,
